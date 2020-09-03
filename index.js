40d5875ae7d837478140175fe59dceea1a0cdb62
Danish Ali <danishrao.am@gmail.com>
2020-09-03 22:13:31 +0500

    finished with dev and prod

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/example');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
+require('./routes/example')(app);
+
 app.get('/', (req,res) => {
-    res.send("hello there")
+res.send("hello there")
 })
 
 const PORT = process.env.PORT || 5000
